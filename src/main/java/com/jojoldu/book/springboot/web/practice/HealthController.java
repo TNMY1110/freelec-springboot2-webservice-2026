c3bd756ab3f2bd30ce4869c871686188e99f3deb
unknown <searain1110@gmail.com>
2026-04-27 16:18:03 +0900

    /health 엔드포인트 추가

--- src/main/java/com/jojoldu/book/springboot/web/practice/HealthController.java
+++ src/main/java/com/jojoldu/book/springboot/web/practice/HealthController.java
@@ -1,4 +1,18 @@
 package com.jojoldu.book.springboot.web.practice;
 
-public class HealthController {
+
+import com.jojoldu.book.springboot.web.practice.dto.HealthResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController
+{
+    @GetMapping("/health")
+    public HealthResponseDto health()
+    {
+        return new HealthResponseDto("OK", LocalDateTime.now());
+    }
 }
